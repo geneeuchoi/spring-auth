e19e8972c1df8600e6af9c9ea78caac901c47f9b
EugeneChoi <100894921+geneeuchoi@users.noreply.github.com>
2024-05-29 13:50:56 +0900

    쿠키와 세션

---
+++ src/main/java/com/sparta/springauth/auth/AuthController.java
@@ -0,0 +1,76 @@
+package com.sparta.springauth.auth;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+@RestController
+@RequestMapping("/api")
+public class AuthController {
+
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+
+    @GetMapping("/create-cookie")
+    public String createCookie(HttpServletResponse res) {
+        addCookie("Robbie Auth", res);
+
+        return "createCookie";
+    }
+
+    @GetMapping("/get-cookie")
+    //쿠키 가져오기
+    public String getCookie(@CookieValue(AUTHORIZATION_HEADER) String value) {
+        System.out.println("value = " + value);
+
+        return "getCookie : " + value;
+    }
+
+    @GetMapping("/create-session")
+    //suvlet에 요청들어왔을때 request 객체 만들어줌
+    public String createSession(HttpServletRequest req) {
+        // 세션이 존재할 경우 세션 반환, 없을 경우 새로운 세션을 생성한 후 반환
+        HttpSession session = req.getSession(true);
+
+        // 세션에 저장될 정보 Name - Value 를 추가합니다.
+        session.setAttribute(AUTHORIZATION_HEADER, "Robbie Auth");
+
+        return "createSession";
+    }
+
+    @GetMapping("/get-session")
+    public String getSession(HttpServletRequest req) {
+        // 세션이 존재할 경우 세션 반환, 없을 경우 null 반환
+        HttpSession session = req.getSession(false);
+
+        //Object -> String 캐스팅
+        String value = (String) session.getAttribute(AUTHORIZATION_HEADER); // 가져온 세션에 저장된 Value 를 Name 을 사용하여 가져옵니다.
+        System.out.println("value = " + value);
+
+        return "getSession : " + value;
+    }
+
+    public static void addCookie(String cookieValue, HttpServletResponse res) {
+        try {
+            cookieValue = URLEncoder.encode(cookieValue, "utf-8").replaceAll("\\+", "%20"); // Cookie Value 에는 공백이 불가능해서 encoding 진행
+
+            Cookie cookie = new Cookie(AUTHORIZATION_HEADER, cookieValue); // Name-Value
+            cookie.setPath("/");
+            cookie.setMaxAge(30 * 60); //만료기한
+
+            // Response 객체에 Cookie 추가
+            //클라이언트에 반환
+            //res에 addCookie라는 메서드(쿠키 담는 공간) 존재
+            res.addCookie(cookie);
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+}
